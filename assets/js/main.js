d871d9817dd42819424e97126b6815ab4253fd55
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-28 18:40:20 +0100

    Bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,66 @@
+// Press play to start
+document.getElementById('play').addEventListener('click', function(){
 
-// Create grid
-for(let i = 1; i <= 100; i++){
-    // Create an "li" node:
-    const li = document.createElement("li");
+    // Take difficulty level
+    difficulty = Number(document.getElementById('difficulty').value);
+    //console.log(difficulty);
 
-    // Add a class to "li":
-    li.classList.add("box")
+    // Declare bombNumb for switch
+    let boxNumb;
 
-    // Create a text node:
-    let liText = document.createTextNode(i);
+    // Switch game mode
+    switch (difficulty) {
+        //Easy
+        case 1:
+            boxNumb = 100;
+            document.getElementById('grid').style.width = '500px'
+        break;
+
+        // Medium
+        case 2:
+            boxNumb = 81;
+            document.getElementById('grid').style.width = '450px'
+        break;
+    
+        //Hard
+        case 3:
+            boxNumb = 49;
+            document.getElementById('grid').style.width = '350px'
+        break;
+    }
+
+    //Cler grid for when u change game difficulty
+    document.getElementById('grid').innerHTML= ' ';   
+
+    // Create grid for the difficulty
+    for(let i = 1; i <= boxNumb; i++){
+        // Create an "li" node:
+        const li = document.createElement("li");
+
+        // Add a class to "li":
+        li.classList.add("box")
+
+        // Create a text node:
+        let liText = document.createTextNode(i);
         
-    // Append the text node to the "li" node:
-    li.appendChild(liText);
+        // Append the text node to the "li" node:
+        li.appendChild(liText);
    
-    // Append the "li" node to the list:
-    document.getElementById("grid").appendChild(li);
-}
+        // Append the "li" node to the list:
+            document.getElementById("grid").appendChild(li);
+    }
 
-const boxArray = document.getElementsByClassName('box')
-//console.log(boxArray);
+    // Take list of box as array
+    const boxArray = document.getElementsByClassName('box');
+    //console.log(boxArray);
 
-// Add event listener for any box
-for (let i = 0; i < boxArray.length; i++) {
-    const element = boxArray[i];
+    // Add event listener for any box with for cycle in array
+    for (let i = 0; i < boxArray.length; i++) {
+        const element = boxArray[i];
 
-    element.addEventListener('click', function(e) {
-        element.classList.toggle('blue')
+        element.addEventListener('click', function(e) {
+        element.classList.toggle('blue');
         console.log(element.textContent);
-    });
-}
+        });
+    }   
+});
